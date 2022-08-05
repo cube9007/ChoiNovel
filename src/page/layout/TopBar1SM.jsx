d85d818f3216c98b4aedc2f5e784b15984c316a8
최원호 <choiwonho@choewonhoui-MacBookPro.local>
2022-08-05 15:27:51 +0900

    home1

--- src/page/layout/TopBar1SM.jsx
+++ src/page/layout/TopBar1SM.jsx
@@ -7,19 +7,16 @@ import { Home, FormatListBulleted, CreditCard } from "@mui/icons-material";
 function TopBarSM() {
   return (
     <div>
-        <Box className="flex justify-center">
+        <Box className="topbarSM">
             <div className="topbar">
                 <Link to='/home' className="topbar-selected">
                     <Home/>
-                    <p>홈</p>
                 </Link>
                 <Link to='/list'>
                     <FormatListBulleted />
-                    <p>둘러보기</p>
                 </Link>
                 <Link to ='/card'>
                     <CreditCard/>
-                    <p>정액제</p>
                 </Link>
             </div>
         </Box>
